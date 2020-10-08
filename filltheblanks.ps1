bd9dcc49e3657e81904f135a93b194db93da3566
Tryfolos <tryfolos@gmail.com>
2020-10-08 11:03:42 +0200

    change

---
+++ filltheblanks.ps1
@@ -0,0 +1,6 @@
+﻿$word = Read-Host -Prompt "Give me a word"
+$adjective = Read-Host -Prompt "Give me an adjective"
+$verb = Read-Host -Prompt "Give me a verb"
+
+
+Write-Output "A $word pirate wants to sing in a $word band, but needs to find a group to sing with. What was that $adjective sound? He knew he heard something and there it was again. It was music. $adjective music and he loved it! The pirate stopped $verb and just $verb. Then he started $verb his toes. Before he knew it, he was $verb his head and his arms too. Now he wanted to sing. He had to sing; he couldn’t help himself. When he opened his mouth, out came a $adjective sound. He was quite possibly the $adjective singer in the whole world."
